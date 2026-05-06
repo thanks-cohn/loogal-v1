c071267ac53bee37bd65b3f6c56c544da455c9c2
thanks-cohn <officialmahasbiz@gmail.com>
2026-05-06 10:36:06 -0600

    Track platform abstraction header

---
+++ include/loogal/platform.h
@@ -0,0 +1,14 @@
+#ifndef LOOGAL_PLATFORM_H
+#define LOOGAL_PLATFORM_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+int loogal_platform_file_size(const char *path, uint64_t *out_size);
+int loogal_platform_file_exists(const char *path);
+int loogal_platform_file_readable(const char *path);
+int loogal_platform_mkdir(const char *path);
+int loogal_platform_copy_file(const char *src, const char *dst);
+uint64_t loogal_platform_now_ns(void);
+
+#endif
