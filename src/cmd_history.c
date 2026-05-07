327cc5be5b8a087502dcf47467597175cc63a1e8
thanks-cohn <officialmahasbiz@gmail.com>
2026-05-06 19:30:13 -0600

    Use platform mkdir helper in history

--- src/cmd_history.c
+++ src/cmd_history.c
@@ -1,14 +1,13 @@
 #define _XOPEN_SOURCE 700
 #include "loogal.h"
+#include "loogal/platform.h"
 #include "jsonout.h"
 #include "history.h"
 
 #include <ctype.h>
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 
@@ -73,11 +72,11 @@ static int mkdir_p_simple(const char *path) {
     for (char *p = tmp + 1; *p; p++) {
         if (*p == '/') {
             *p = 0;
-            if (mkdir(tmp, 0755) != 0 && errno != EEXIST) return -1;
+            if (loogal_platform_mkdir(tmp) != 0) return -1;
             *p = '/';
         }
     }
-    if (mkdir(tmp, 0755) != 0 && errno != EEXIST) return -1;
+    if (loogal_platform_mkdir(tmp) != 0) return -1;
     return 0;
 }
 
