0bcd1701ab2b063c01bc4f960bd8830cb27e2dfc
thanks-cohn <officialmahasbiz@gmail.com>
2026-05-07 18:41:32 -0600

    Complete persistent single-file watch indexing

--- src/cmd_watch_event.c
+++ src/cmd_watch_event.c
@@ -17,7 +17,7 @@ static void usage(void) {
 }
 
 int loogal_cmd_watch_event(int argc, char **argv) {
-    if (argc < 3 || strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
+    if (argc < 4 || strcmp(argv[2], "--help") == 0 || strcmp(argv[2], "-h") == 0) {
         usage();
         return 1;
     }
@@ -25,22 +25,22 @@ int loogal_cmd_watch_event(int argc, char **argv) {
     LoogalWatchEvent ev;
     memset(&ev, 0, sizeof(ev));
 
-    if (strcmp(argv[1], "--created") == 0) {
+    if (strcmp(argv[2], "--created") == 0) {
         ev.type = LOOGAL_WATCH_EVENT_CREATED;
-        snprintf(ev.path, sizeof(ev.path), "%s", argv[2]);
-    } else if (strcmp(argv[1], "--modified") == 0) {
+        snprintf(ev.path, sizeof(ev.path), "%s", argv[3]);
+    } else if (strcmp(argv[2], "--modified") == 0) {
         ev.type = LOOGAL_WATCH_EVENT_MODIFIED;
-        snprintf(ev.path, sizeof(ev.path), "%s", argv[2]);
-    } else if (strcmp(argv[1], "--deleted") == 0) {
+        snprintf(ev.path, sizeof(ev.path), "%s", argv[3]);
+    } else if (strcmp(argv[2], "--deleted") == 0) {
         ev.type = LOOGAL_WATCH_EVENT_DELETED;
-        snprintf(ev.path, sizeof(ev.path), "%s", argv[2]);
-    } else if (strcmp(argv[1], "--moved") == 0) {
-        if (argc < 4) {
+        snprintf(ev.path, sizeof(ev.path), "%s", argv[3]);
+    } else if (strcmp(argv[2], "--moved") == 0) {
+        if (argc < 5) {
             usage();
             return 1;
         }
         ev.type = LOOGAL_WATCH_EVENT_MOVED;
-        snprintf(ev.old_path, sizeof(ev.old_path), "%s", argv[2]);
+        snprintf(ev.old_path, sizeof(ev.old_path), "%s", argv[3]);
         snprintf(ev.path, sizeof(ev.path), "%s", argv[3]);
     } else {
         usage();
